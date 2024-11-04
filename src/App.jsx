077671218c4d48438b131b5894dce7714d0884d9
MennaEbied <mennahesham253@gmail.com>
2024-11-04 18:47:18 +0200

    Setting up pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
+import Login from "./pages/login/login";
+import Home from "./pages/home/home";
+import List from "./pages/list/list";
+import Single from "./pages/single/single";
+import New from "./pages/new/new";
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div className="container">
-        
+        <BrowserRouter>
+          <Routes>
+            <Route path="/">
+              <Route index element={<Home/>}/>
+              <Route path="login" element={<Login/>}/>
+              <Route path="users">
+                <Route index element={<List/>}/>
+                <Route path="userId" element={<Single/>}/>
+                <Route path="new" element={<New/>}/>
+              </Route>
+              <Route path="products">
+                <Route index element={<List/>}/>
+                <Route path="productId" element={<Single/>}/>
+                <Route path="new" element={<New/>}/>
+              </Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   )
