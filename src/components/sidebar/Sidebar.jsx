eaa1229639b445dc8dedeaf985746a8055a99d67
MennaEbied <mennahesham253@gmail.com>
2024-11-04 18:59:56 +0200

    Setting up components

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,14 @@
+import "./sidebar.scss"
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+        <div className="top"></div>
+        <div className="center"></div>
+        <div className="bototm"></div>
+      
+    </div>
+  )
+}
+
+export default Sidebar
