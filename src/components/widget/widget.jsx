006525770a07661cc1ade4454d1477c01749190d
MennaEbied <mennahesham253@gmail.com>
2024-11-05 16:37:49 +0200

    Adding widget component

---
+++ src/components/widget/widget.jsx
@@ -0,0 +1,24 @@
+import "../widget/widget.scss";
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+
+function Widget() {
+  return (
+    <div className="widget">
+        <div className="left">
+            <span className="title">USERS</span>
+            <span className="counter">54654</span>
+            <span className="link">See All Users</span>
+        </div>
+        <div className="right">
+            <div className="percentage">
+             <KeyboardArrowUpIcon/>
+              20%
+            </div>
+            <PersonOutlineIcon className="icon"/>
+        </div>
+    </div>
+  )
+}
+
+export default Widget;
