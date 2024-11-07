2c05cb745e93486e65a3e0bc185fbe009e8e7783
MennaEbied <mennahesham253@gmail.com>
2024-11-07 12:10:20 +0200

    Adding link and new user button

--- src/components/dataTable/DataTable.jsx
+++ src/components/dataTable/DataTable.jsx
@@ -1,7 +1,8 @@
 import "./datatable.scss"
 import { DataGrid } from '@mui/x-data-grid';
 import Paper from '@mui/material/Paper';
-import { userColumns,userRows} from "../../dataTableSource"
+import { userColumns,userRows} from "../../dataTableSource";
+import { Link } from "react-router-dom";
 
 
 const paginationModel = { page: 0, pageSize: 5 };
@@ -10,13 +11,21 @@ const DataTable = () => {
   const actionColumn=[{field: "action", headerName: "Action", width:200, renderCell:()=>{
     return(
       <div className="cellAction">
-        <div className="viewButton">View</div>
+        <Link to="/users/test" style={{textDecoration :"none"}}>
+          <div className="viewButton">View</div>
+        </Link>
         <div className="deleteButton">Delete</div>
       </div>
     )
   }}]
   return (
     <div className="dataTable">
+      <div className="dataTableTitle">
+        Add New User
+        <Link to="/users/new" className="link">
+          Add New
+        </Link>
+      </div>
        <Paper sx={{ height: 400, width: '100%' }}>
       <DataGrid
         rows={userRows}
